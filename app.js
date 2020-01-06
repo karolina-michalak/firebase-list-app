040c27592e99138f756ab124093c211d2c9251ef
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-06 22:40:43 +0100

    getting data from firestore in console

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 db.collection('books').get()
     .then((snapshot) => {
         snapshot.docs.forEach(doc => {
-            console.log(doc)
+            console.log(doc.data())
         })
     })
