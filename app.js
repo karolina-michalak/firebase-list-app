9dee5a7354c0fb98fc57fe3d9fd3bc564908969d
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-06 22:46:40 +0100

    getting data

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+const bookList = document.querySelector('#book-list')
+
+//create element and render book
+function renderBook(doc){
+    let li = document.createElement('li')
+    let title = document.createElement('span')
+    let author = document.createElement('span')
+
+    li.setAttribute('data-id', doc.id)
+    title.textContent = doc.data().title
+    author.textContent = doc.data().author
+
+    li.appendChild(title)
+    li.appendChild(author)
+
+    bookList.appendChild(li)
+}
+
 db.collection('books').get()
     .then((snapshot) => {
         snapshot.docs.forEach(doc => {
-            console.log(doc.data())
+            renderBook(doc)
         })
     })
