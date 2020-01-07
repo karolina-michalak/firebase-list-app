47f08e97d002f51e6e8bf0742925eff360fff5cb
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-07 18:10:45 +0100

    addex remove element

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ function renderBook(doc){
     let li = document.createElement('li')
     let title = document.createElement('span')
     let author = document.createElement('span')
+    let x = document.createElement('div')
 
     li.setAttribute('data-id', doc.id)
     title.textContent = doc.data().title
     author.textContent = doc.data().author
+    x.textContent = 'x'
 
     li.appendChild(title)
     li.appendChild(author)
+    li.appendChild(x)
 
     bookList.appendChild(li)
 }
